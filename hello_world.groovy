c1569a7f6e7a079419db0e9f713466823e0b47fb
flickwoo <flickwoo@7dcc7362-a928-0410-a033-9a33e6eab302>
2008-07-30 06:32:06 +0000

    Meh.
    git-svn-id: svn+ssh://stagbeer.com/home/flickwoo/repos@175 7dcc7362-a928-0410-a033-9a33e6eab302

---
+++ hello_world.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+public class Greetings {
+    public static void main(String[] args) {
+	for(int i = 0; i < 3; i++) {
+	    System.out.println("ho ");
+	}
+	System.out.println("Merry Groovy!");
+    }
+}
