784028549a3373eff1d8f8bfcc132abf4f5f630e
flickwoo <flickwoo@7dcc7362-a928-0410-a033-9a33e6eab302>
2008-08-31 02:05:54 +0000

    Added some more date stuff
    git-svn-id: svn+ssh://stagbeer.com/home/flickwoo/repos@187 7dcc7362-a928-0410-a033-9a33e6eab302

--- date_increment.groovy
+++ date_increment.groovy
@@ -1,7 +1,9 @@
 #!/usr/bin/env groovy
 d = new Date() + 1
-late_time = new Date(d.getTime() + 1000)
+late_time = new Date(d.getTime() + 1000 * 60 * 60 * 24)
 
 println late_time
 
 println d
+// Groovy flakes out if you try to construct with a decimal
+another_time = new Date(d.getTime() + (1000 * 60 * 2.5).toInteger())
