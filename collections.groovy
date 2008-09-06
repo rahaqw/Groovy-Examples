804f421f2f05b65352ce8c7b09dc63683efcf538
Nathan Neff <nathan.neff@gmail.com>
2008-09-05 22:42:26 -0500

    Fixed collections.groovy's comments

--- collections.groovy
+++ collections.groovy
@@ -1,17 +1,13 @@
 #!/usr/bin/env groovy
-# Purpose of this script is to show how to exclude / include stuff
-# from lists
+// Purpose of this script is to show how to exclude / include stuff
+// from lists
 class Person {
     String name
     String address
 }
-def person = Person.new(name:'Bert', address:'101')
-def person2 = Person.new(name:'Ernie', address:'101')
+def person = new Person(name:'Bert', address:'101')
+def person2 = new Person(name:'Ernie', address:'101')
 
 def plist = []
 plist << person
 plist << person2
-
-plist.
-
-
