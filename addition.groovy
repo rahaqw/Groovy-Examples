17a709f6b7ce6b355ae77cc31f232b41d0c9bfc3
Nathan Neff <nathan.neff@gmail.com>
2009-01-16 23:22:07 -0600

    Example addition

---
+++ addition.groovy
@@ -0,0 +1,19 @@
+moop = [:]
+if (moop['1']) {
+  moop['1'] += 1
+}
+else {
+  moop['1'] = 1
+}
+
+println moop['1'] + "\n"
+
+
+if (moop['1']) {
+  moop['1'] += 1
+}
+else {
+  moop['1'] = 1
+}
+
+println moop['1']
