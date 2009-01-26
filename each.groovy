3c24371d002b3967ec14b56f39e3d29e047490e8
Nathan Neff <nathan.neff@gmail.com>
2009-01-26 10:22:57 -0700

    Integer conversion on arguments is necessary?

--- each.groovy
+++ each.groovy
@@ -1,5 +1,17 @@
 #!/usr/bin/env groovy
+// 4
 (1 .. 100).each {
     def val = it
     println "I'm running ${val}<br />"
+}
+
+def numTimes = 1
+
+if (this.args) {
+    numTimes = this.args[0].toInteger()
+}
+
+println "Num times is ${numTimes}"
+(1 .. numTimes).each {
+    println "Whee ${it}"
 }
