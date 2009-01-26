6b98ed504966699da7a677734105d69a1b8932ff
Nathan Neff <nathan.neff@gmail.com>
2009-01-26 10:23:23 -0700

    "Replace all" demo.

--- replace.groovy
+++ replace.groovy
@@ -1,3 +1,13 @@
-def s = "Foo Bar Baz"
-s = s.replaceAll(" ", "\\\\ ")
+def s = "Foo Bar Baz <servername>"
+s = s.replaceAll("<servername>", "foo")
 println s
+
+// Open a file, and replace all <servername>
+// with foo
+s = ""
+def fis = new File('replace.groovy')
+s = fis.text.replaceAll("<servername>", "foo")  
+
+println s
+// Leave the below comment IN! <servername>
+// <servername> should now be foo
