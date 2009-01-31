8a3509faca34741daa3514483e575e0e01cfd54b
Nathan Neff <nathan.neff@gmail.com>
2009-01-31 00:13:06 -0600

    Case statement to handle arguments.

--- arguments.groovy
+++ arguments.groovy
@@ -1,7 +1,25 @@
 #!/usr/bin/env groovy
 for (a in this.args) {
-  println("Argument: " + a)
+    println("Argument: " + a)
 }
+
+def foo = 'Foo'
+def bar = 'Bar'
+
+// Go through each argument, and print index, and value.
+this.args.eachWithIndex { obj, index ->
+    println "Arg: ${index} is ${obj}"
+    switch ( index ) {
+        case 0:
+            foo = obj
+            break
+        case 1:
+            bar = obj
+            break
+    }
+}
+
+println "Foo is ${foo} Bar is: ${bar}"
 
 if (this.args) {
     println "First arg is: ${this.args[0]}"
