967c48d4ed87d250aacb9f54a48dda63353a1fb2
Nathan Neff <nathan.neff@gmail.com>
2009-05-09 01:41:35 -0500

    Add html

---
+++ string_misc.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/env groovy
+
+/* 
+ *  Script: string_misc.groovy
+ *
+ * Purpose: <+purpose+>
+ *
+ *   Usage: groovy string_misc.groovy <+usage+> 
+ *
+ *  Author: <+author+>
+ */
+// How to get your stuff back out of regex,
+// at least in a simple match/fetch scenario.
+def fullName = "Bart Simpson"
+m = fullName =~ /(\w+)/
+assert m.getCount() == 2
+assert m[0][0] == "Bart"
+assert m[1][0] == "Simpson"
