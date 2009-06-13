1a99ca83e6601dd918ab883f80e176c1b055e591
Nathan Neff <nathan.neff@gmail.com>
2009-06-13 00:35:35 -0500

    Something.

---
+++ regex.groovy
@@ -0,0 +1,70 @@
+#!/usr/bin/env groovy
+// Yet another regex groovy script
+// Specializes in OR regexes and minimal matching
+
+def testString = "foo"
+// Obvious OR match
+assert (testString =~ /(foo|bar)/)
+testString = "bar"
+assert (testString =~ /(foo|bar)/)
+
+
+// This won't match, because baz is not in foo or bar
+testString = "baz"
+assert (!(testString =~ /(foo|bar)/))
+
+// Same expression matches anything containing "foo" or "bar"
+testString = "fooBarBaz"
+assert (testString =~ /(foo|bar)/)
+// Test exact matches
+assert (!("fooBarBaz" =~ /^(foo|bar)$/))
+assert ("foo" =~ /^(foo|bar)$/)
+testString = "BigbarBeeBoo"
+assert (testString =~ /(foo|bar)/)     
+
+// Match fooBarBaz but not XXXfooBarBaz.
+// Only diff from above regexes is caret ^
+testString = "fooBarBaz"
+assert (testString =~ /^(foo|bar)/)
+testString = "XXXfooBarBaz"
+assert (!(testString =~ /^(foo|bar)/))
+// The ?! means "negative lookahead assertion", which means
+// match any foo or bar that isn't preceeded directly with the start
+// of the string http://perldoc.perl.org/perlre.html#Extended-Patterns
+
+// The w+$ characters means that characters must follow foo or bar,
+// which seems a bit redundant, since we've already limited our search to
+// anything with foo or bar that's NOT at the end of the string.
+// To show that the \w+$ is useless, I test it below.
+
+assert (!("foo" =~ /^(?!foo|bar)\w+$/))         // with \w+$
+assert (!("foo" =~ /^(?!foo|bar)/))
+assert (!("fooBarBaz" =~ /^(?!foo|bar)\w+$/))   // with \w+$
+assert (!("fooBarBaz" =~ /^(?!foo|bar)/))
+assert (!("bar" =~ /^(?!foo|bar)\w+$/))         // with \w+$
+assert (!("bar" =~ /^(?!foo|bar)/))
+assert (!("barZZZZZ" =~ /^(?!foo|bar)\w+$/))    // with \w+$
+assert (!("barZZZZZ" =~ /^(?!foo|bar)/))
+// This will match
+assert ("ZZZZZbarZZZZZ" =~ /^(?!foo|bar)\w+$/)
+assert ("ZZZZZbarZZZZZ" =~ /^(?!foo|bar)/)
+assert ("grover" =~ /^(?!foo|bar)\w+$/)
+assert ("grover" =~ /^(?!foo|bar)/)
+assert ("FOO" =~ /^(?!foo|bar)\w+$/)
+assert ("FOO" =~ /^(?!foo|bar)/)
+
+// A simpler way?
+assert ("dooBarBaz" =~ /^(?!foo|bar)\w+$/)
+
+// Simpler way is to just not match the start of string + (foo or bar)
+assert (!("dooBarBaz" =~ /^(foo|bar)/))
+
+
+// assert (!("foofooBarBaz" =~ /^(?!foo|bar)\w+$/))
+// assert (testString =~ /(foo|bar)\w+$/)
+// // This one does NOT match, because
+// // baz is in the OR clause
+// assert (testString =~ /(?!foo|bar|baz)\w+$/)
+// assert (testString =~ /^(?!foo|bar|baz)\w+$/)
+// 
+// assert (testString =~ /(?!foo)\w+$/)
---
+++ string_comparison.groovy
@@ -0,0 +1,5 @@
+#!/usr/bin/env groovy
+def str = "123"
+def lng = 123
+
+assert str.toString() == lng.toString()
