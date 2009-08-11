a70731c523ea641b6adfc1ddcac686794b69f55b
Nathan Neff <nathan.neff@gmail.com>
2009-08-11 15:50:41 -0500

    Joining hashes

--- hashmap.groovy
+++ hashmap.groovy
@@ -1,4 +1,35 @@
 #!/usr/bin/env groovy
+
+// Show how to join two hashes
+def record1 = [
+    firstName : 'barn',
+    lastName : 'smith',
+    zipcode : '22222'
+]
+def record2 = [
+    phone : '976-EVIL',
+    zipcode : '11111']
+
+
+
+// record2.each { key, value ->
+//     record1[key] = value
+// }
+
+record1 += record2
+
+// Print firstName and zipcode from this record.
+def outputFields = [ 'firstName', 'zipcode' ]
+
+println outputFields.collect {
+    record1[it]
+}.join("\n")
+
+// Assert that record2's fields were added to record1
+assert record1.phone == '976-EVIL'
+assert record1.zipcode == '11111'
+assert record1.firstName == 'barn'
+
 myMap = [freature:'Clusres', version:'1.1']
 
 
