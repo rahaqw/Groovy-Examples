bda67a44c94fe725d35c15a0d488b945bd532f4a
Nathan Neff <nathan.neff@gmail.com>
2009-08-26 16:00:48 -0500

    List copying example.

---
+++ list_copying.groovy
@@ -0,0 +1,24 @@
+#!/usr/bin/env groovy
+
+/* 
+ *  Script: list_copying.groovy
+ *   Usage: list_copying.groovy
+ * Purpose:
+ *  Author: Crazy Nate
+ * History: 
+ */
+
+
+def list1 = ['foo', 'bar', 'baz']
+def list2 = list1
+
+// Removing / changing things in list2 also does it to list1
+assert list2.remove(0) == 'foo'
+assert list1[0] == 'bar'
+
+// Use clone method to create two distinct lists
+list1 = ['foo', 'bar', 'baz']
+list2 = list1.clone()
+
+assert list2.remove(0) == 'foo'
+assert list1[0] == 'foo'
