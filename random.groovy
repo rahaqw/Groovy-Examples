ff741eb7acc816aea84f394aa81cd9d359ab349b
Nathan Neff <nathan.neff@gmail.com>
2009-08-26 16:00:10 -0500

    Random groovy script.

--- random.groovy
+++ random.groovy
@@ -1,6 +1,11 @@
 #!/usr/bin/env groovy
 def seed = new Random()
+println seed.nextInt(100)
 println seed.nextInt(100000)
 
 
 println "Next double: " + (seed.nextDouble() * 100000).toInteger()
+
+(0 .. 100).each {
+    println seed.nextInt(3)
+}
