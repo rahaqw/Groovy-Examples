80c76aaf70d6268e24290fa4b6c49665a48cd429
Nathan Neff <nathan.neff@gmail.com>
2009-08-26 16:00:38 -0500

    Math examples.

---
+++ math.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+
+/* 
+ *  Script: math.groovy
+ *   Usage: math.groovy
+ * Purpose:
+ *  Author: Crazy Nate
+ * History: 
+ */
+
+// modulo, etc.
+ass ert 34 + 33 == 67    && 34.plus( 33 ) == 67
+assert 34L - 21L == 13L && 34L.minus( 21L ) == 13L
+assert 3 * 31 == 93     && 3.multiply( 31 ) == 93
+assert 23 % 3 == 2      && 23.mod( 3 ) == 2
+assert 3**2 == 9        && 3.power( 2 ) == 9
