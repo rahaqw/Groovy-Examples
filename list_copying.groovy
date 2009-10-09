c0c492abec70480d8fcb4ea304a73f2bf98f915f
Nathan Neff <nathan.neff@gmail.com>
2009-10-09 10:24:24 -0500

    Testing the comparison for arrays/lists groovy

--- list_copying.groovy
+++ list_copying.groovy
@@ -19,6 +19,11 @@ assert list1[0] == 'bar'
 // Use clone method to create two distinct lists
 list1 = ['foo', 'bar', 'baz']
 list2 = list1.clone()
+list3 = list1.clone()
+
+assert list2.equals(list3)
+assert list2 == list3
+assert !(list2 == list3.reverse())
 
 assert list2.remove(0) == 'foo'
 assert list1[0] == 'foo'
