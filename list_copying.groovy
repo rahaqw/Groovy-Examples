5b4fdae10e235825a806efe4f606e86e7c905f6b
Nathan Neff <nathan.neff@gmail.com>
2009-10-11 20:32:06 -0500

    Use "is" to compare whether lists are the same object

--- list_copying.groovy
+++ list_copying.groovy
@@ -21,9 +21,14 @@ list1 = ['foo', 'bar', 'baz']
 list2 = list1.clone()
 list3 = list1.clone()
 
+// Clone / copy list -- compare lists using == or .equals
+// == will delegate to the .equals method -- DGG chapter "The Groovy Language"
 assert list2.equals(list3)
 assert list2 == list3
 assert !(list2 == list3.reverse())
+assert !(list2.is(list3))
+def list4 = list2
+assert (list2.is(list4))
 
 assert list2.remove(0) == 'foo'
 assert list1[0] == 'foo'
