bc6db5c63e00ac6af141f59857dd8d250aa35872
Nathan Neff <nathan.neff@gmail.com>
2009-12-01 14:51:01 -0600

    Groovy substring example

---
+++ substring.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+/* 
+ *  Script: substring.groovy
+ *   Usage: substring.groovy
+ * Purpose:
+ *  Author: Crazy Nate
+ * History: 
+ */
+
+def s = "Something"
+assert s.substring(0,1) == 'S'
