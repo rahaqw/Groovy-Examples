58455701741160fe13d7a1256c09fe692e5b27a8
Nathan Neff <nathan.neff@gmail.com>
2010-01-03 20:18:53 -0600

    Example of max() and min() methods on ArrayList

--- collectionMax.groovy
+++ collectionMax.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 def list = ['foo', 'bar', 'baz', 'quk']
+assert list instanceof ArrayList
 
 // max and min return the last/first
 // item in a list when the list is sorted
@@ -9,3 +10,5 @@ assert list.min() == 'bar'
 def newlist = list.sort()
 assert newlist[0] == list.min()
 assert newlist[-1] == list.max()
+
+assert newlist instanceof ArrayList
