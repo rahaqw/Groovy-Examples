be4bd108d2063e579dda3f03da6ade3d1c4972ba
Nathan Neff <nathan.neff@gmail.com>
2010-01-27 11:28:29 -0600

    split, regex, join, collect examples

--- join.groovy
+++ join.groovy
@@ -11,3 +11,19 @@
  *  Author: <+author+>
  */
 assert [1, 2, 3].join('-') == '1-2-3'
+
+
+// filter out nulls
+assert [1, 2, 3, null, 4, null, 5, 69].findAll {
+    it != null
+}.join('-') == '1-2-3-4-5-69'
+
+def cc = 'CamelCaseWord'
+def newList = cc.split("(?=[A-Z])").findAll({ it.length()}).collect {
+    it[0].toUpperCase() + it[1 .. -1].toLowerCase()
+}
+assert newList[0] == 'Camel'
+assert newList[1] == 'Case'
+assert newList[2] == 'Word'
+
+assert 'Camel Case Word' == newList.join(' ')
