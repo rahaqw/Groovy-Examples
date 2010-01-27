e69d16c70a9a14b31677371413a3567cb01b0e8e
Nathan Neff <nathan.neff@gmail.com>
2010-01-27 11:30:52 -0600

    Another split example.  Camel case words.

--- string_split.groovy
+++ string_split.groovy
@@ -15,3 +15,25 @@ assert list[0] == "foo"
 assert list[1] == "bar"
 assert list[2] == "baz"
 assert list[3] == "quk"
+
+
+// Split on CamelCaseWords
+// Use the (?=blah) to find the position just
+// in *front* of the capital letter
+// a.k.a. Zero length border matches.
+list = 'birthDate'.split("(?=[A-Z])")
+assert list[0] == 'birth'
+assert list[1] == 'Date'
+
+// notice that an empty string is pre-pended to the list
+list = 'CamelCaseWords'.split("(?=[A-Z])")
+assert list[0] == ''
+assert list[1] == 'Camel'
+assert list[2] == 'Case'
+assert list[3] == 'Words'
+
+
+
+def packageName = 'org.bworks.bworksdb.Enrollio'
+def splitted = packageName.split("\\.")
+assert splitted[-1] == 'Enrollio'
