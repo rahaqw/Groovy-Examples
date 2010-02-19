7c62c568124f454426ce661977cd47f8837f6bab
Nathan Neff <nate@lemmy2.gateway.2wire.net>
2010-02-19 09:40:28 -0600

    Case insensitive searching using the 'contains' method.

--- string_regex.groovy
+++ string_regex.groovy
@@ -6,6 +6,12 @@ email = '++++foo@foo.com'
 email = email.replaceFirst(/^\++/, '')
 assert email == 'foo@foo.com'
 
+mystr = "Female"
+assert mystr.contains("F")
+assert mystr.toLowerCase().contains("f")
+
+mystr = "Male"
+assert ! mystr.contains("F")
 
 mystr = '"1062","Incremental","Backup","ahha04_managed_contegix_com","File System","192","23 Jul 2008","04:00:00","23 Jul 2008","04:07:34","Backup Completed with warnings","1.8GB","7.1MB",'
 // positive assertion
