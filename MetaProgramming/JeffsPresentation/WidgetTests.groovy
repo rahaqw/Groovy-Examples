6fb8d7c477d2e6a3afac7c93b42dba8f71450e80
Nathan Neff <nathan.neff@gmail.com>
2011-02-02 13:02:13 -0600

    Add missing method on the fly

--- MetaProgramming/JeffsPresentation/WidgetTests.groovy
+++ MetaProgramming/JeffsPresentation/WidgetTests.groovy
@@ -1,7 +1,13 @@
 class WidgetTests extends GroovyTestCase {
     void testSomething() {
         Widget.metaClass.methodMissing = { String methodName, args ->
-            methodName
+            println "in methodMissing for ${methodName}"
+            def impl = {
+                methodName
+            }
+            Widget.metaClass."${ methodName }" = impl
+            // invoke new method (closure)
+            impl()
         }
 
         def w = new Widget()
