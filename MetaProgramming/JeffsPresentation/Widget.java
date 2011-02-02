8c2a5a4d327222f5152695879a96ee7a1334ddf7
Nathan Neff <nathan.neff@gmail.com>
2011-02-02 12:57:52 -0600

    Add blank methodMissing to java class

--- MetaProgramming/JeffsPresentation/Widget.java
+++ MetaProgramming/JeffsPresentation/Widget.java
@@ -1,2 +1,2 @@
-class Widget {
+public class Widget {
 }
--- MetaProgramming/JeffsPresentation/WidgetTests.groovy
+++ MetaProgramming/JeffsPresentation/WidgetTests.groovy
@@ -1,7 +1,16 @@
 class WidgetTests extends GroovyTestCase {
     void testSomething() {
+        Widget.metaClass.methodMissing = { String methodName, args ->
+        }
+
         def w = new Widget()
         assertNotNull w
+        println w.boo()
+        println w.boo()
+        println w.boo()
+        println w.boo()
+        println w.boo()
+
     }
 }
 
