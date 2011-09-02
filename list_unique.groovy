ff0bbb83bbd5a299edd4c7ddd762833a44d9bbd3
Nathan Neff <nathan.neff@gmail.com>
2011-09-02 18:23:32 -0500

    unique list

--- list_unique.groovy
+++ list_unique.groovy
@@ -1,4 +1,3 @@
 #!/usr/bin/env groovy
-# Unique method returns list with all duplicates removed
-def l = [1, 2, 3, 4, 5, 1, 2, 10]
-println l.unique()
+def l = ["Illinois", "Missouri", "Illinois", "New York"]
+assert ["Illinois", "Missouri", "New York"] == l.unique()
