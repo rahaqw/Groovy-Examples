0b24c52955ebc56bdc224551b43d07e7f491e8eb
Nathan Neff <nathan.neff@gmail.com>
2011-09-02 18:23:06 -0500

    epoch and base64

---
+++ base64.groovy
@@ -0,0 +1,7 @@
+#!/usr/bin/env groovy
+// From Mr Haki
+def s = 'Argh, Groovy you say, mate?'
+String encoded = s.bytes.encodeBase64().toString()
+assert 'QXJnaCwgR3Jvb3Z5IHlvdSBzYXksIG1hdGU/' == encoded
+byte[] decoded = encoded.decodeBase64()
+assert s == new String(decoded)
