9895dc53f0080addc5b2cc4876af04450ecc386f
Nathan Neff <nathan.neff@gmail.com>
2011-10-04 17:21:41 -0500

    treemap more stuff

--- replace2.groovy
+++ replace2.groovy
@@ -1,3 +1,4 @@
 #!/usr/bin/env groovy
 
 println "\$foo\$foo".replaceAll(/\$/, "") 
+assert "new-york" == "New York".replace("\\s","")
--- treemap.groovy
+++ treemap.groovy
@@ -1,6 +1,9 @@
 #!/usr/bin/env groovy
-// find way to get a list of the values
-// in a map, sorted by value
+/* 
+ * find way to get a list of the values
+ * in a map, sorted by value
+ * tags:  treemap
+*/ 
 
 def someMap = [
     'lesson_1' : 10,
@@ -25,4 +28,19 @@ assert keys[3] == 'lesson_4'
 assert keys[4] == 'lesson_6'
 assert keys[5] == 'lesson_59'
 assert keys[6] == 'lesson_0'
+
+
+def foo = [:] as TreeMap
+foo['a'] = "bob"
+foo['b'] = "bob"
+foo['z'] = "bob"
+foo['Z'] = "bob"
+foo['c'] = "bob"
+
+def keyz = foo.keySet().toArray()
+assert keyz[0] == 'Z'
+assert keyz[1] == 'a'
+assert keyz[2] == 'b'
+assert keyz[3] == 'c'
+assert keyz[4] == 'z'
 
