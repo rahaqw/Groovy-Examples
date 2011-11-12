b5c95511aad64db6b96baadeead17131d32e54fd
Nathan Neff <nathan.neff@gmail.com>
2011-11-11 18:57:21 -0600

    Another split example

--- src/split.groovy
+++ src/split.groovy
@@ -6,3 +6,13 @@ def str = "123"
 
 assert str[0] == "1"
 
+
+def string = "your;mom,is;a,"
+def list = string.split(/,|;/)
+assert list.size() == 4
+assert list[0] == "your"
+assert list[1] == "mom"
+assert list[2] == "is"
+assert list[3] == "a"
+
+
