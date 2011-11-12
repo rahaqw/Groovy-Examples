9117dbba8b1dc1d5e7b1c6eb915847e787f863a1
Nathan Neff <nathan.neff@gmail.com>
2011-11-11 19:02:07 -0600

    More create hash map from list goodness

--- createHashmapFromList.groovy
+++ createHashmapFromList.groovy
@@ -1,4 +1,6 @@
 #!/usr/bin/env groovy
+// tags: inject hash keys key each iterator loop create list
+
 def listOfThings = [
     [ name : 'Bob', age: 1 ],
     [ name : 'Doug' , age: 2],
@@ -6,14 +8,23 @@ def listOfThings = [
     [ name : 'Dookie' , age: 4]
 ]
 
+def existingHash = [
+    'Snarf' : [ name : 'Snarf', age: 300], 
+    'Bloganje' : [ name : 'Bloganje Sr.', age : 12 ]
+]
 
-def hashOfThings = listOfThings.inject([:]) { hash, thing ->
+def newHash = listOfThings.inject(existingHash) { hash, thing ->
     hash << [ (thing.name) : thing ]
 }
 
+assert newHash.size() == 6
+assert newHash['Bob'].age == 1
+assert newHash['Bob'].name == 'Bob'
 
-assert hashOfThings.size() == 4
-assert hashOfThings['Bob'].age == 1
-assert hashOfThings['Bob'].name == 'Bob'
+// ensure that our existing stuff is still there
+assert newHash['Snarf'].name == 'Snarf'
+assert newHash['Snarf'].age == 300
+assert newHash['Bloganje'].name == 'Bloganje Sr.'
+assert newHash['Bloganje'].age == 12
 
 
